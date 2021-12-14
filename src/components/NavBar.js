433683ba6f36c0986c0f9deb85ccf231b857eb33
gian251 <samontegian251@yahoo.com>
2021-12-14 19:42:15 +0800

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ const NavBar = () => {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/myfirstreact/home">ICS2608</a>
+                    <a class="navbar-brand" href="/myfirstreact">ICS2608</a>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link" href="/myfirstreact/home">Home</a>
+                            <a class="nav-link" href="/myfirstreact">Home</a>
                             <a class="nav-link" href="/myfirstreact/employees">Employees</a>
                             <a class="nav-link" href="/myfirstreact/add">Add/Update Employee</a>
                             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
